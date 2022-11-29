424df47a356d34d0eb91c3c8183706f5ca2b55e7
will <will@invertedv.com>
2022-11-29 07:51:58 -0700

    pre-trip commit #2

--- scripts/fitter.sh
+++ scripts/fitter.sh
@@ -5,7 +5,7 @@ read -p "User: " user
 read -s -p "Password: " password
 echo ""
 
-./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/mod.gom
 ./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEven.gom
 ./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEvenStrat.gom
+./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInStrat.gom
 
