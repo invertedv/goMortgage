a912726453f37cc7b44e0e99eb5987298edea039
will <will@invertedv.com>
2022-12-12 22:33:51 -0700

    working on .gom files

--- scripts/fitter.sh
+++ scripts/fitter.sh
@@ -5,10 +5,10 @@ read -p "User: " user
 read -s -p "Password: " password
 echo ""
 
-./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/dq.gom
+#./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/prepayScore.gom
 
 #./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/mod.gom
-#./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEven.gom
-#./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEvenStrat.gom
+./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEven.gom
+./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEvenStrat.gom
 #./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInStrat.gom
 
