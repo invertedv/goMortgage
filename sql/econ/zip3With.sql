9f794c6a49800c4d8a1803e84a4e8f86b906444b
will <will@invertedv.com>
2022-12-14 15:34:43 -0700

    added freddie-bug fix

--- sql/econ/zip3With.sql
+++ sql/econ/zip3With.sql
@@ -2,21 +2,21 @@
 WITH e AS (
     SELECT
         zip3,
-    month,
-    avg(zip3Hpi) AS hpi,
-    sum(unempRate * lbrForce) / sum(lbrForce) AS unempRate,
-    sum(lbrForce) AS lbrForceTot,
-    max(mortFix30) AS mortFix30,
-    max(mortFix15) AS mortFix15,
-    max(treas10) AS treas10,
-    max(q10) AS income10,
-    max(q25) AS income25,
-    max(q50) AS income50,
-    max(q75) AS income75,
-    max(q90) AS income90,
-    max(msaName) = '' ? max(state) : max(msaName) AS msaNameLoc,
-    max(fcDays) AS fcDays,
-    max(fcType) AS fcType
-FROM
+        month,
+        avg(zip3Hpi) AS hpi,
+        sum(unempRate * lbrForce) / sum(lbrForce) AS unempRate,
+        sum(lbrForce) AS lbrForceTot,
+        max(mortFix30) AS mortFix30,
+        max(mortFix15) AS mortFix15,
+        max(treas10) AS treas10,
+        max(q10) AS income10,
+        max(q25) AS income25,
+        max(q50) AS income50,
+        max(q75) AS income75,
+        max(q90) AS income90,
+        max(msaName) = '' ? max(state) : max(msaName) AS msaNameLoc,
+        max(fcDays) AS fcDays,
+        max(fcType) AS fcType
+  FROM
     econGo.final
 GROUP BY zip3, month)
