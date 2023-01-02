a53c11a2b68987ebdb09c05cfe821240da74c015
will <will@invertedv.com>
2023-01-02 08:53:48 -0700

    added key to keys.txt

--- scripts/fitter.sh
+++ scripts/fitter.sh
@@ -5,14 +5,14 @@ read -p "User: " user
 read -s -p "Password: " password
 echo ""
 
-#./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/dq.gom
+./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/dq.gom
 #./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/prepayScore.gom
 
 
 #./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/mod.gom
-./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEven.gom
+#./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEven.gom
 #./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInStrat.gom
-./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEvenStrat.gom
+#./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/allInEvenStrat.gom
 
 #./goMortgage -user $user -pw $password -specs /home/will/GolandProjects/goMortgage/scripts/netPro.gom
 
